8364717568f69e4d460efd3c7e30e30168af4ffe
Josefa Cordeiro Da Silva <133906172+codename-FlawZ@users.noreply.github.com>
2025-07-23 18:32:31 -0300

    update

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,26 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY='re_YskWT5PL_4AGaKoxHX6BRCDCNPWTfWh2K')
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
-    const { email, contactType, firstName, lastName, message } = await req.json()
+  const { email, contactType, firstName, lastName, message } = await req.json();
 
-    try {
-        await resend.emails.send({
-            from: 'Acme <onboarding@resend.dev>',
-            to: 'contato.osmarditosoficial@gmail.com',
-            subject: `Novo contato: ${contactType}`,
-            text: `
-             Nome: ${firstName} ${lastName} 
-             Email: ${email} 
-             Tipo: ${contactType} 
-             Mensagem: ${message}
-            `,
-        })
-        return NextResponse.json({ success: true }, { status: 200 })
-    } catch (error) {
-        console.error(error)
-        return NextResponse.json({ error: 'Erro ao enviar email' }, { status: 500 })
-    }
+  try {
+    const data = await resend.emails.send({
+      from: 'contato@osmarditos.com', 
+      to: 'contato.osmarditosoficial@gmail.com',
+      subject: `Novo contato: ${contactType}`,
+      text: `
+        Nome: ${firstName} ${lastName}
+        Email: ${email}
+        Tipo: ${contactType}
+        Mensagem: ${message}
+      `,
+    });
+
+    console.log("E-mail enviado:", data); 
+    return NextResponse.json({ success: true }, { status: 200 });
+  } catch (error) {
+    console.error("Erro ao enviar e-mail:", error); 
+    return NextResponse.json({ error: "Erro ao enviar email" }, { status: 500 });
+  }
 }
