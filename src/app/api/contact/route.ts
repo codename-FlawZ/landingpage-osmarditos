e70a4fee2102ae0ee3b53407de1aa371e9bd88bd
Josefa Cordeiro Da Silva <133906172+codename-FlawZ@users.noreply.github.com>
2025-07-27 01:12:16 -0300

    Update route.ts

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: NextRequest) {
 
   try {
     const data = await resend.emails.send({
-      from: 'contato.osmarditos.com', 
+      from: process.env.FROM_EMAIL!,
       to: 'contato.osmarditosoficial@gmail.com',
       subject: `Novo contato: ${contactType}`,
       text: `
